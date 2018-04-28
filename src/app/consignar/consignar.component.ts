ccc1c8b29b8458865fe0e8027f5b418ce9b961ad
Frank Edward Daza González <frank.daza2@gmail.com>
2018-04-28 02:16:45 -0500

    Se agrega campo nuevo de número de cédula al módulo de consignación.

--- src/app/consignar/consignar.component.ts
+++ src/app/consignar/consignar.component.ts
@@ -12,6 +12,7 @@ export class ConsignarComponent implements OnInit {
 
   txtNumeroCuenta: string;
   txtValor: Number;
+  txtCedula: Number;
   txtMensajeError: string;
 
   constructor(
@@ -32,18 +33,26 @@ export class ConsignarComponent implements OnInit {
 
   consignar(): void {
     if (this.txtNumeroCuenta != "") {
-      this.usuarioService.setConsignar(this.txtNumeroCuenta, this.txtValor)
-      .subscribe(response => {
-        this.txtMensajeError = response.mensajeError;
-        if (response.codigoError == 0) {          
-          console.log(this.txtMensajeError);
+      if (this.txtValor > 0) {
+        if (this.txtCedula != null || this.txtCedula > 1000) {
+          this.usuarioService.setConsignar(this.txtNumeroCuenta, this.txtValor, this.txtCedula)
+          .subscribe(response => {
+            this.txtMensajeError = response.mensajeError;
+            if (response.codigoError == 0) {          
+              console.log(this.txtMensajeError);
+            } else {
+              console.log(this.txtMensajeError);
+            }
+          });
         } else {
-          console.log(this.txtMensajeError);
-        }
-      });
+          this.txtMensajeError = "Ingrese un número de cédula válido";  
+        }        
+      } else {
+        this.txtMensajeError = "Ingrese un número mayor que cero";  
+      }  
     } else {
       this.txtMensajeError = "Ingrese un número de cuenta";
-    }   
+    }      
   }
 
   homepage(): void {
--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -21,12 +21,12 @@ export class UsuarioService {
     return this.http.get<Usuario>(this.urlUsuario);  
   }
 
-  setConsignar(numeroCuenta: String, valor: Number) : Observable<Respuesta> {
-    this.urlUsuario = 'http://127.0.0.1:8080/banco/rest/controllers/transaccion/consignarTransaccion/' + numeroCuenta + '/' + this.loginSesion + '/' + valor;
+  setConsignar(numeroCuenta: string, valor: Number, numeroCedula: Number) : Observable<Respuesta> {
+    this.urlUsuario = 'http://127.0.0.1:8080/banco/rest/controllers/transaccion/consignarTransaccion/' + numeroCuenta + '/' + this.loginSesion + '/' + valor + '/' + numeroCedula;
     return this.http.get<Respuesta>(this.urlUsuario);
   }
 
-  setRetirar(numeroCuenta: String, valor: Number) : Observable<Respuesta> {
+  setRetirar(numeroCuenta: string, valor: Number) : Observable<Respuesta> {
     this.urlUsuario = 'http://127.0.0.1:8080/banco/rest/controllers/transaccion/retiroTransaccion/' + numeroCuenta + '/' + this.loginSesion + '/' + valor;
     return this.http.get<Respuesta>(this.urlUsuario);
   }
